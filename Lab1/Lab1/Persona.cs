288ae343c7a1a9d2dae97008669d90c3ee7f3b5a
Nicolás Jofré <nrjofre@miuandes.cl>
2020-03-17 17:16:12 -0300

    1

---
+++ Lab1/Lab1/Persona.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Persona
+    {
+        public string nombre;
+        public string apellido;
+
+        public void Lanzar()
+        {
+            var Random = new Random();
+            var value = Random.Next(0, 3);
+            Console.WriteLine(value);
+        }
+
+        //Constructor clase persona con atributos nombre y apellido
+        public Persona(string nombre, string apellido)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+        }
+    }
+}
---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Parte 1 Crear instancia en blanco para Persona (con parametros ya que al crear el constructor para la parte 2 tira error
+
+            Persona p1 = new Persona("","");
+            p1.Lanzar();
+
+            //Parte 2 objeto Persona con atributos bob kunga
+            Persona p2 = new Persona("Bob","Kunga");
+
+            //para que la consola no se cierre :D
+            Console.ReadLine();
+        }
+    }
+}
